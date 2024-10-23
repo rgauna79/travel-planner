8df7af11bc61e6fe90f724d576d7e79de8c7fcb9
RobertG <49545278+rgauna79@users.noreply.github.com>
2024-10-23 06:31:17 +0000

    fix: navigate on homepage

--- client-frontend/src/pages/HomePage.jsx
+++ client-frontend/src/pages/HomePage.jsx
@@ -1,7 +1,9 @@
 import { useAuth } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
   const { isAuthenticated } = useAuth();
+  const navigate = useNavigate();
   return (
     <div
       className="container mt-5 d-flex flex-column justify-content-center align-items-center"
@@ -16,14 +18,14 @@ const HomePage = () => {
       <div>
         <button
           className="btn btn-primary btn-lg mx-2"
-          onClick={() => (window.location.href = "/trips")}
+          onClick={() => navigate("/trips")}
         >
           View My Trips
         </button>
         {!isAuthenticated && (
           <button
             className="btn btn-outline-secondary btn-lg mx-2"
-            onClick={() => (window.location.href = "/register")}
+            onClick={() => navigate("/register")}
           >
             Get Started
           </button>
